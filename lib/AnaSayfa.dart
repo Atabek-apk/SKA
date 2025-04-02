e479172534380c7ebb87bdccef53ec6265350613
Atabek-apk <bagtyyarov.atabek@gmail.com>
2025-04-02 15:16:36 +0500

    duz

--- lib/AnaSayfa.dart
+++ lib/AnaSayfa.dart
@@ -86,7 +86,7 @@ class HomeScreen extends StatelessWidget {
                         boxShadow: [
                           BoxShadow(
                             color: Colors.deepPurpleAccent.withOpacity(0.2), // Hafif mor gölge
-                            blurRadius: 3, // Yumuşaklık az 1111zzzzzzzz
+                            blurRadius: 3, // Yumuşaklık az 33333333333
                             spreadRadius: 0, // Yanlardan yayılma yok
                             offset: Offset(0, 3), // Yalnızca aşağıya doğru
                           ),
